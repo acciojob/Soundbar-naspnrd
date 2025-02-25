5e412f7fc07dc10369cdaa1c6b2fb4406dcc8834
Neeraj Chaudhary <36655557+naspnrd@users.noreply.github.com>
2025-02-25 23:12:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+
+const btns = Array.from(document.querySelectorAll(".btn"));
+console.log(btns)
+// ["appluase", "boo", "gasp", "tada", "victory", "wrong"]
+
+function playSounds(btn) {
+	btn.addEventListener("click", () => {
+		stopSounds();
+		document.getElementById(btn.innerText).play();
+	})
+}
+
+
+function stopSounds(params) {
+	for(let i = 0; i < btns.length; i++) {
+		const sound = document.getElementById(btns[i].innerText);
+		console.log({sound})
+		sound.pause();
+		sound.currentTime = 0;
+	}
+}
+
+for(let i = 0; i < btns.length; i++) {
+		playSounds(btns[i]); 
+	}
+document.querySelector(".stop").addEventListener('click', () => {
+	stopSounds()
+})
